3978a982d656b8b5e586cdd18853291a2ceea325
takechiyocn@osaka <otagao@gmail.com>
2014-03-07 01:22:07 +0900

    stage for commit

---
+++ tricky/special_cli.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# ★★★★★★★★★★★★★★★★★★★★ #
+# M1: !$
+# get last parameter of last command
+# eg: mkdir gemini/dir_test
+#     cd !$ <==> cd gemini/dir_test
+# effect in command line
+# ★★★★★★★★★★★★★★★★★★★★ #
+
+# ★★★★★★★★★★★★★★★★★★★★ #
+# M2: ll
+# alias ll='ls -alF'
+#       -F, --classify
+#              append indicator (one of */=>@|) to entries
+# *: executable file
+# /: directory
+# =: socket file
+# >: TODO
+# @: link file
+# |: FIFO file
+# add nothing: regular file
+# ★★★★★★★★★★★★★★★★★★★★ #
