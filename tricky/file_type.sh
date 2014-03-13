42f0d2a73591d72ca544e3e17e1b196b28fef247
takechiyocn@osaka <otagao@gmail.com>
2014-03-14 00:42:02 +0900

    get file's type && special character added

---
+++ tricky/file_type.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# ★★★★★★★★★★★★★★★★★★★★ #
+# alias ll='ls -alF'
+#       -F, --classify
+#              append indicator (one of */=>@|) to entries
+#
+# *: executable file
+# /: directory
+# =: socket file
+# >: TODO
+# @: link file
+# |: FIFO file, PIPE file
+# add nothing: regular file
+# ★★★★★★★★★★★★★★★★★★★★ #
