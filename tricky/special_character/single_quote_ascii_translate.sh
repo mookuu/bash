f63d64fe355dfdc450afa0d3165c0b39cef72384
Kohata@osk <otagao@gmail.com>
2014-03-14 12:06:27 +0900

    single quote added

---
+++ tricky/special_character/single_quote_ascii_translate.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+oct_quote=$'\042'                   # octal number 42 is "(double quotes)
+hex_quote=$'\x22'                  # hex number is "(double quotes)
+
+echo "oct_quote:$oct_quote"
+echo "hex_quote:$hex_quote"
