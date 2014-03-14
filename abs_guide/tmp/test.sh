9c1a92f038c97c36d9a0ac70031475aed095cb8f
Kohata@osk <otagao@gmail.com>
2014-03-14 19:03:12 +0900

    usage of singlie quote $'' added

--- abs_guide/tmp/test.sh
+++ abs_guide/tmp/test.sh
@@ -1,23 +1,7 @@
-#! /bin/bash
+#!/bin/sh
 
-# recursive multiplication
-
-function factorial
-{
-        local tmp_val=0
-        local next_num=0
-        if (( $1<=1 )); then
-                result=1
-                return 0
-        else
-                (( next_num=$1 - 1 ))
-                factorial $next_num
-                (( tmp_val=$1 * $result ))
-                result=$tmp_val
-                return 0
-        fi
-}
-
-factorial $1
-
-echo "Factorial of $1 is:$result"
+if [ -z $1 ]; then
+        echo "parameter null"
+else
+        echo "parameter OK"
+fi
--- tricky/test.sh
+++ tricky/test.sh
@@ -1,46 +1,15 @@
-#!/bin/bash
+#!/bin/bash -x
+test_char ()
+{
+case "$1" in
+    [[:print:]] ) echo "$1 is a printable character.";;&
+    [[:alnum:]] ) echo "$1 is an alpha/numeric character.";;& # v
+[[:alpha:]] ) echo "$1 is an alphabetic character.";;& # v
+[[:lower:]] ) echo "$1 is a lowercase alphabetic character.";;&
+[[:digit:]] ) echo "$1 is an numeric character.";& # |
 
-#
-# This file echoes a bunch of color codes to the
-# terminal to demonstrate what's available. Each
-# line is the color code of one forground color,
-# out of 17 (default + 16 escapes), followed by a
-# test use of that color on all nine background
-# colors (default + 8 escapes).
-#
+%%%@@@@@ ) echo "********************************"
+            ;; # v
 
-T='gYw' # The test text
-
-echo -e "\n          test  40m   41m   42m   43m   44m   45m   46m   47m";
-
-for FGs in 'm' '1m' '30m' '1;30m' '31m' '1;31m' '32m' \
-    '1;32m' '33m' '1;33m' '34m' '1;34m' '35m' '1;35m' \
-    '36m' '1;36m' '37m' '1;37m';
-do
-        FG=${FGs// /}
-        case ${#FGs} in
-            1)
-                FGs="[$FGs]    "
-                ;;
-            2)
-                FGs="[$FGs]   "
-                ;;
-            3)
-                FGs="[$FGs]  "
-                ;;
-            4)
-                FGs="[$FGs] "
-                ;;
-            5)
-                FGs="[$FGs]"
-                ;;
-        esac
-
-        echo -en " $FGs \033[$FG $T "
-        for BG in 40m 41m 42m 43m 44m 45m 46m 47m;
-        do
-                echo -en "$EINS \033[$FG\033[$BG $T \033[0m";
-        done
-        echo ;
-done
-echo
+esac
+}
