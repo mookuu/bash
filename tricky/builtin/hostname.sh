bf5c05d8f280024f86e02d20560d59ffc160bfbd
Kohata@osk <otagao@gmail.com>
2014-03-25 19:39:36 +0900

    hosts file added

--- tricky/builtin/hostname.sh
+++ tricky/builtin/hostname.sh
@@ -4,3 +4,17 @@
 # gethostname() sets the BASH internal variable $HOSTNAME
 #
 echo $HOSTNAME
+
+# show current hostname
+hostname
+
+# change hostname 
+hostname test
+
+
+# all configurations in /etc/hosts
+# 1: network ip address
+# 2: hostname.domain_name
+# 3: hostname
+
+127.0.0.1	# loop address
