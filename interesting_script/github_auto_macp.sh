c68069c66fdfa7be0fcb7cb998b9fff60d5a987c
Kohata@osk <otagao@gmail.com>
2014-04-15 19:16:17 +0900

    Kohata@nj 2014-04-15 19:16:17

--- interesting_script/github_auto_macp.sh
+++ interesting_script/github_auto_macp.sh
@@ -1,7 +1,8 @@
-#!/bin/bash -x
+#!/bin/bash
 
 #  Automatically add, commit, push files to github
-#
+#  Update
+#       Parameter check added @2014-04-15 by H
 
 RET=0
 D_COM_TRUE=1
@@ -12,7 +13,11 @@ E_ADD_ERR=88
 E_COMMIT_ERR=89
 E_PUSH_ERR=90
 
-GIT_COMMENT="-m"
+GIT_ADD_FILE="-a"
+GIT_ADD_FILE1="--add"
+GIT_REMOVE_FILE="-r"
+GIT_REMOVE_FILE1="--remove"
+GIT_COMMENT_FLAG="-m"
 GIT_COMMENT_CUSTOMIZE=$D_COM_FALSE
 PATH_LOG=/home/kohata/work/cs/log/trace.log
 
@@ -21,19 +26,82 @@ PATH_LOG=/home/kohata/work/cs/log/trace.log
 #  Usage
 usage()
 {
-	echo "Usage:"
-	echo "	`basename $0` [-m] comment file(s)|directory"
-	exit $E_PARA_ERR
+        echo "USAGE"
+	echo "       `basename $0` [options] file|directory"
+        echo "OPTIONS"
+	echo "       -a, --add"
+	echo "          Add file contents to the index."
+	echo "       -r, --remove"
+	echo "          Remove files from the working tree and from the index."
+	echo "       -m"
+	echo "          Add comment to current commit."
+        # TODO: gg -a file(s)|directory -r file(s)|directory # unecessary
+#	exit $E_PARA_ERR
 }
 
+#  Option chk
+opt_chk()
+{
+    :
+}
 
-#  Add files to stage
 #  Parameter chk
-if [ -z $1 ]; then
-	usage
-fi
+para_chk()
+{
+        # Null parameter chk
+	if [ -z "$1" ]; then
+                echo "[DBG]: Null parameter!" && echo
+		usage		# print usage
+	fi
+
+        #
+	# TODO: -m comment
+        while [ -n "$1" ]
+        do
+                case "$1" in
+                    $GIT_ADD_FILE|$GIT_ADD_FILE1)
+                        echo "[DBG]: Add mode"
+                        # gg -a file|directory
+                        # gg -a -m "customize-comment" file|dir
+                        # TODO: gg -a files -r files
+                        # gg -a -m -r
+                        break
+                        ;;
+                    $GIT_REMOVE_FILE|$GIT_REMOVE_FILE1)
+                        echo "[DBG]: Remove mode"
+                        break
+                        ;;
+                    $GIT_COMMENT_FLAG)
+                        echo "[DBG]: Customize comment mode."
+                        break
+                        ;;
+                    # TODO: default add mode
+                    *)
+                        if [ ${1:0:1} = - ]; then
+                                echo "[DBG]: '$1' unknown mode." && echo
+                                usage
+                        else    # TODO: file check?
+                                echo "[DBG]: default mode(add mode)."
+                        fi
+                        break
+                        ;;
+                esac
+        done
+}
+
+para_chk $1 $2 $3 $4 $5 $6
+echo "before exit"
+exit
+
+#  Parameter chk
+para_chk $1 $2 $3 $4 $5 $6
+
+#  Option chk
+opt_chk
+
+
 #  Customize comment
-if [ $GIT_COMMENT = $1 ]; then
+if [ $GIT_COMMENT_FLAG = $1 ] || []; then
 	str=$2
 	echo "[DBG]: [L38] $2"
 	# If parameters contains '-m' but without customize comment
@@ -57,6 +125,8 @@ if [ $GIT_COMMENT = $1 ]; then
 	fi
 	echo "[DBG]: Shift the parameter"
 fi
+
+#  TODO: files add directory
 #  File(s)
 if [ -f $1 ]; then
 	while [ ! -z $1 ]
