31c3e93250650e37e2e86079f69d8863456db2de
Kohata@osk <otagao@gmail.com>
2014-04-15 12:52:00 +0900

    Kohata@nj 2014-04-15 12:52:00

--- interesting_script/test.sh
+++ interesting_script/test.sh
@@ -6,5 +6,6 @@ echo The \# here does not begin a comment.
 echo The # here begins a comment.
 echo ${PATH#*:} # Parameter substitution, not a comment.
 echo $(( 2#101011 )) # Base conversion, not a comment.
+echo "Github test"
 
 # Thanks, S.C.
